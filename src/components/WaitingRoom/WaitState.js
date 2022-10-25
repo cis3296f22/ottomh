76455d46686168bec6919dcf9eb53616ff576c53
Mo2artGit <tuj76813@temple.edu>
2022-10-25 12:06:01 -0400

    Create WaitState.js

---
+++ src/components/WaitingRoom/WaitState.js
@@ -0,0 +1,20 @@
+import Button from 'react-bootstrap/Button';
+
+export default function WaitState() {
+    return(
+        <div>
+            <h1>OTTOMH</h1>
+            <div>
+                <h2>Code:</h2>
+                <p>"room code"</p>
+                <Button variant="primary">Copy URL</Button>
+                <br/>
+            </div>
+            <div>
+                <h2>Players joined:</h2>
+                <p>"Players"</p>
+            </div>
+            <Button variant="primary" type="submit">Start</Button>
+        </div>
+    );
+}
