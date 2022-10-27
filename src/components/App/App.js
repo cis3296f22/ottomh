ab042afed3ccf0cb67e60eb8f4a6363a133fc738
Jennifer Lieu <jennilieu@gmail.com>
2022-10-27 13:28:58 -0400

    Fix: Join button enters same watiing room as host

--- src/components/App/App.js
+++ src/components/App/App.js
@@ -4,15 +4,21 @@ import { BrowserRouter, Routes, Route } from 'react-router-dom';
 import { Home, Join, WaitState } from '../';
 
 export const App = () => {
-  let [lobbyId, setLobbyId] = useState("hello");
+  let [lobbyId, setLobbyId] = useState("none");
+  let [username, setUsername] = useState("none");
 
   return (
     <div className="App">
+      <p>
+        State information (for debugging):
+        <br />Lobby id: {lobbyId}
+        <br />Username: {username}
+      </p>
       <BrowserRouter>
         <Routes>
           <Route exact path="/" element={<Home openLobby={setLobbyId} />} />
-          <Route path="/join" element={<Join />}></Route>
-          <Route path={"/lobbies/" + lobbyId} element={<WaitState id={lobbyId} />} />
+          <Route path="/join" element={<Join openLobby={setLobbyId} />}></Route>
+          <Route path={"/lobbies/:lobbyId"} element={<WaitState id={lobbyId} />} />
         </Routes>
       </BrowserRouter>
     </div>
