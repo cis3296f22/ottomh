0ac1506d114f5c27a54a0d6e1dbb5cc51063a63b
Landen Lloyd <oldestoflloyd@gmail.com>
2022-10-27 20:30:22 -0400

    Blank page for voting

---
+++ src/components/Voting/Voting.js
@@ -0,0 +1,9 @@
+import './votingStyles.css';
+
+export const Voting = () => {
+    return (
+        <div>
+            Hello, Voting!
+        </div>
+    );
+}
--- src/components/index.js
+++ src/components/index.js
@@ -2,3 +2,4 @@ export * from './App/App.js';
 export * from './Home/Home.js';
 export * from './Join/Join.js';
 export * from './WaitState/WaitState.js';
+export * from './Voting/Voting.js';
