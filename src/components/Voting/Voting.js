8b70079033d4c3a449fe755ba86b912ae8a1c5c3
Landen Lloyd <oldestoflloyd@gmail.com>
2022-10-31 19:08:15 -0400

    Allow words in list to be crossed off

--- src/components/Voting/Voting.js
+++ src/components/Voting/Voting.js
@@ -1,10 +1,33 @@
 import './votingStyles.css';
 import { Container, Row, ListGroup } from 'react-bootstrap';
 import { GamePageTimer } from '../';
+import { useState } from 'react';
 
 export const Voting = ({onTimeover, words}) => {
-    let wordList = words.map((word) => 
-        <ListGroup.Item key={word}>{word}</ListGroup.Item>);
+    let onTimerStop = (_) => {
+        onTimeover();
+    }
+
+    // Create an array of boolean that stores whether or not a word is crossed
+    let [crossed, setCrossed] = useState(
+        Array.from(Array(words.length)).map((_) => false) // Construct array of falses
+    );
+
+    let wordList = words.map((word, index) => 
+        <ListGroup.Item action 
+                onClick={() => setCrossed((crossed) => {
+                    let newCrossed = crossed.slice(0);
+                    newCrossed[index] = !newCrossed[index];
+                    return newCrossed;
+                })} 
+                key={word + crossed[index]}
+                variant={crossed[index] ? 'danger' : ''}>
+
+            {!crossed[index] && word}
+
+            {crossed[index] && <del>{word}</del>}
+
+        </ListGroup.Item>);
 
     return (
         <Container fluid className='m-5 w-auto text-center'
@@ -15,7 +38,7 @@ export const Voting = ({onTimeover, words}) => {
                 </ListGroup>
             </Row>
             <Row className='h-25 align-items-center'>
-                {GamePageTimer((_) => onTimeover())}
+                {GamePageTimer(onTimerStop)}
             </Row>
         </Container>
     );
