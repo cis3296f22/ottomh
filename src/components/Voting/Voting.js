c10ae7fd2708607dc18973ab467109e2b246218a
Landen Lloyd <oldestoflloyd@gmail.com>
2022-10-31 15:21:37 -0400

    Add timer to voting page

--- src/components/Voting/Voting.js
+++ src/components/Voting/Voting.js
@@ -1,15 +1,22 @@
 import './votingStyles.css';
 import { Container, Row, ListGroup } from 'react-bootstrap';
+import { GamePageTimer } from '../';
 
-export const Voting = ({words}) => {
+export const Voting = ({onTimeover, words}) => {
     let wordList = words.map((word) => 
         <ListGroup.Item key={word}>{word}</ListGroup.Item>);
 
     return (
-        <Container fluid className='votingContainer m-5 overflow-auto w-auto'>
-            <ListGroup>
-                {wordList}
-            </ListGroup>
+        <Container fluid className='m-5 w-auto text-center'
+                style={{'height': '95vh'}}>
+            <Row className='h-75 overflow-auto'>
+                <ListGroup>
+                    {wordList}
+                </ListGroup>
+            </Row>
+            <Row className='h-25 align-items-center'>
+                {GamePageTimer((_) => onTimeover())}
+            </Row>
         </Container>
     );
 }
