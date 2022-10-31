aeed7ebf3b4af63252601a835f958e6bb4178f75
Landen Lloyd <oldestoflloyd@gmail.com>
2022-10-31 14:14:06 -0400

    scrollable list of words

--- src/components/Voting/Voting.js
+++ src/components/Voting/Voting.js
@@ -1,9 +1,15 @@
 import './votingStyles.css';
+import { Container, Row, ListGroup } from 'react-bootstrap';
 
-export const Voting = () => {
+export const Voting = ({words}) => {
+    let wordList = words.map((word) => 
+        <ListGroup.Item key={word}>{word}</ListGroup.Item>);
+
     return (
-        <div>
-            Hello, Voting!
-        </div>
+        <Container fluid className='votingContainer m-5 overflow-auto w-auto'>
+            <ListGroup>
+                {wordList}
+            </ListGroup>
+        </Container>
     );
 }
