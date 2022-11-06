f257f811122008788286b3cd5d8ae7d5b10300e4
Mo2artGit <tuj76813@temple.edu>
2022-11-06 11:47:35 -0500

    Moved open socket from Wait to Lobby

--- src/components/LobbyPage/LobbyPage.js
+++ src/components/LobbyPage/LobbyPage.js
@@ -16,6 +16,22 @@ export const LobbyPage = () => {
     let cat = category[Math.floor(Math.random() * category.length)];
     let letter = characters[Math.floor(Math.random() * characters.length)];
 
+    let ws;
+
+    if (window.location.protocol === 'https:') {
+        ws = new WebSocket(`wss://${window.location.host}/sockets/${lobbyId}`);
+    } else {
+        ws = new WebSocket(`ws://${window.location.host}/sockets/${lobbyId}`);
+    }
+
+    ws.onopen = (_) => {
+        alert("websocket is open now");
+    }
+
+    ws.onclose = (_) => {
+        alert("websocket is closed now");
+    }
+
     return (
         <div className="container-fluid h-100">
             {stage === "waitingRoom" && <WaitState onStart={() => setStage("playGame")} id={lobbyId} />}
