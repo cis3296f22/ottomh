b83270ede9e1629f19e99e7a46d8d49ba2b8563c
tun31876 <tun31876@temple.edu>
2022-11-11 14:56:30 -0500

    stopping timer after reaching 0; adding argument to gamepagetimer

--- src/components/GamePageTimer/GamePageTimer.js
+++ src/components/GamePageTimer/GamePageTimer.js
@@ -1,11 +1,19 @@
 import Button from 'react-bootstrap/Button';
 import { useState, useRef, useEffect } from 'react'
 
-export const GamePageTimer = (setLoading) => {
+
+export const GamePageTimer = (setLoading, time_picked ) => {
 
     const Ref = useRef(null);
-    const [isStart, setIsStart] = useState(true);
-    const [timer, setTimer] = useState('00:60');
+    const [timer, setTimer] = useState(time_picked);
+
+    const split_time = time_picked.split(':',2);
+    const minutes_picked = Number(split_time[0]) 
+    const seconds_picked = Number(split_time[1])
+
+    let full_time_seconds = minutes_picked*60
+    full_time_seconds+=seconds_picked;
+
 
    const getTimeRemaining = (e) => {
         const total = Date.parse(e) - Date.parse(new Date());
@@ -14,15 +22,18 @@ export const GamePageTimer = (setLoading) => {
         return {
             total, minutes, seconds
         };
+    
     }
   
     const startTimer = (e) => {
         let { total, minutes, seconds} 
                     = getTimeRemaining(e);
-        if(seconds === 0){
-            setLoading(false)
-           
-        }            
+        
+        if (minutes == 0 && seconds == 0){
+            clearInterval(Ref.current)
+            setLoading(false) 
+        }
+                 
         if (total >= 0) {
             setTimer(
                 (minutes > 9 ? minutes : '0' + minutes) + ':'
@@ -33,40 +44,40 @@ export const GamePageTimer = (setLoading) => {
    
     
     const clearTimer = (e) => { 
-        setTimer('00:60');
-        if (Ref.current) clearInterval(Ref.current);
+        
+        setTimer(time_picked);
 
         const id = setInterval(() => {
+
             startTimer(e);
-            
-            
+
         }, 1000)
-        
+       
         Ref.current = id;
+        
     }
     
 
     const getDeadTime = () => {
         let deadline = new Date();
-        deadline.setSeconds(deadline.getSeconds() + 60);
+       
+        deadline.setSeconds(deadline.getSeconds() + full_time_seconds);
         return deadline;
     }
 
 
     useEffect(() => {
-        if (isStart){
-            clearTimer(getDeadTime());
-        }
-    }, [isStart]);
 
+        clearTimer(getDeadTime());      
+    
+    }, []);
 
+    
   
     return (
         <div>
-            <Button onClick={() => setIsStart(true)}>Timer <h2>{timer}</h2> </Button>
+            <Button> Timer <h2>{timer}</h2> </Button>
 
         </div>
     )
-
 }
-
