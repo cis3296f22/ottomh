747e34f569c4f9c17908be7949ea0e5d38b7b0a0
Landen Lloyd <oldestoflloyd@gmail.com>
2022-11-14 19:08:53 -0500

    Basic Playwright unit test setup

---
+++ tests/test_host.spec.js
@@ -0,0 +1,21 @@
+import { test, expect } from '@playwright/test';
+
+test('CreateAndLeaveLobby', async ({ page }) => {
+
+  await page.goto('http://localhost:8080/');
+
+  await page.getByText('OTTOMHCreate new lobbyJoin lobby').click();
+
+  await page.getByPlaceholder('Username').fill('Landen');
+
+  page.once('dialog', dialog => {
+    console.log(`Dialog message: ${dialog.message()}`);
+    dialog.dismiss().catch(() => {});
+  });
+  await page.getByPlaceholder('Username').press('Enter');
+  await expect(page).toHaveURL(/http:\/\/localhost:8080\/lobbies\/[a-zA-Z0-9]{6}/);
+
+  await page.getByRole('button', { name: 'Leave Lobby' }).click();
+  await expect(page).toHaveURL('http://localhost:8080/');
+
+});
