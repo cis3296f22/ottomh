5c9d868ff47be99116e49dc1f881d43978ec9199
Landen Lloyd <oldestoflloyd@gmail.com>
2022-11-15 20:27:41 -0500

    Add coverage tests to frontend tests; call all tests from test script

--- tests.sh
+++ tests.sh
@@ -9,7 +9,9 @@ go run server.go &
 
 npx playwright test
 
-# Close down server now that frontend tests are done
-kill -9 $!
-
+npx c8 report --reporter=html -x=["coverage/**","packages/*/test{,s}/**","**/*.d.ts","test{,s}/**","test{,-*}.{js,cjs,mjs,ts,tsx,jsx}","**/*{.,-}test.{js,cjs,mjs,ts,tsx,jsx}","**/__tests__/**","**/{ava,babel,nyc}.config.{js,cjs,mjs}","**/jest.config.{js,cjs,mjs,ts}","**/{karma,rollup,webpack}.config.js","**/.{eslint,mocha}rc.{js,cjs}","node_modules", "webpack"]
+open coverage/index.html
 npx playwright show-report
+
+# Close down any children processes
+kill -TERM $$
--- tests/test_host.spec.js
+++ tests/test_host.spec.js
@@ -1,8 +1,11 @@
 import { test, expect } from '@playwright/test';
-const v8toIstanbul = require('v8-to-istanbul');
-const fs = require('fs');
+const fs = require("fs");
+const url = require("url");
+const path = require("path");
 
 test('CreateAndLeaveLobby', async ({ page }) => {
+  await page.coverage.startJSCoverage();
+
   await page.goto('http://localhost:8080/');
 
   await page.getByText('OTTOMHCreate new lobbyJoin lobby').click();
@@ -18,4 +21,18 @@ test('CreateAndLeaveLobby', async ({ page }) => {
 
   await page.getByRole('button', { name: 'Leave Lobby' }).click();
   await expect(page).toHaveURL('http://localhost:8080/');
+
+  // Courtesy of jfgreffier: https://github.com/microsoft/playwright/issues/9208#issuecomment-1147884893
+  // Get and save V8 coverage
+  const coverage = await page.coverage.stopJSCoverage();
+  const rootPath = path.normalize(`${__dirname}/../build`);
+  const coverageWithPath = coverage.map((entry) => {
+    const fileName = new url.URL(entry.url).pathname;
+    return { ...entry, url: `file:///${rootPath}${fileName}` };
+  });
+
+  fs.writeFileSync(
+    "coverage/tmp/coverage.json",
+    JSON.stringify({ result: coverageWithPath }, null, 2)
+  );
 });
