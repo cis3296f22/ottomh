43e8a9fbc308b241e65872e2f957f92c800affa1
Mo2artGit <tuj76813@temple.edu>
2022-11-17 13:10:24 -0500

    rm unused codes

--- backend/types/scoreAssign.go
+++ backend/types/scoreAssign.go
@@ -1,12 +1,5 @@
 package types
 
-import (
-	"fmt"
-	"net/http"
-
-	"github.com/gin-gonic/gin"
-)
-
 type ScoreList struct {
 	scorem map[string]int
 	Key    string
@@ -27,32 +20,6 @@ func CreateScores() *ScoreList {
 	for key, element := range mapDemo {
 		s.scorem[key] = len(element)
 	}
-	//sort scores by order
 
 	return s
-}
-
-// CODES BELOW NOT USING
-func (s *ScoreList) ScoreCalculator(c *gin.Context) {
-	mapDemo := map[string][]string{
-		"user1": {"one", "two", "three", "four", "five"},
-		"user2": {"one", "two", "three", "four", "five"},
-		"user3": {"one", "two"},
-	}
-
-	//Take map of list words and turn into score map
-	for key, element := range mapDemo {
-		s.scorem[key] = len(element)
-	}
-
-	//temporary verification
-	for key, element := range s.scorem {
-		fmt.Println("User:", key, "=>", "Score:", element)
-	}
-
-	// send map to front end
-	c.JSON(http.StatusOK, gin.H{
-		"Scores": s.scorem,
-	})
-
 }
