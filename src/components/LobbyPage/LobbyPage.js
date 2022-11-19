89458a87ad2842bbf4ebd24de6124ecf70d46b8a
tun31876 <tun31876@temple.edu>
2022-11-19 13:06:06 -0500

    timer 00:30

--- src/components/LobbyPage/LobbyPage.js
+++ src/components/LobbyPage/LobbyPage.js
@@ -71,7 +71,7 @@ export const LobbyPage = () => {
     }
     
     //change timer to 00:60 on deployment to heroku
-    const time_picked = "00:10"
+    const time_picked = "00:30"
 
 
     return (
