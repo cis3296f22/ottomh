90bf126f5f6c5f30097801f09a7855593c03b382
Jennifer Lieu <jennilieu@gmail.com>
2022-11-23 17:44:27 -0500

    Add more categories

--- backend/config/config.go
+++ backend/config/config.go
@@ -8,4 +8,10 @@ var Categories []string = []string{
 	"Places",
 	"Sports",
 	"Movies",
+	"Cities",
+	"Countires",
+	"Places to hangout",
+	"Store names",
+	"Foreign words used in English",
+	"Objects"
 }
