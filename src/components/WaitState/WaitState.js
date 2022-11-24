06b9d7382b4246b9e48c63721955fa2f3cd3a035
Jennifer Lieu <jennilieu@gmail.com>
2022-11-24 01:43:59 -0500

    Style clipobard button

--- src/components/WaitState/WaitState.js
+++ src/components/WaitState/WaitState.js
@@ -1,7 +1,9 @@
 import './waitStateStyle.css';
 import Button from 'react-bootstrap/Button';
+import Modal from 'react-bootstrap/Modal';
 import { useNavigate } from 'react-router-dom';
 import { useStore } from '../../store';
+import { useState } from 'react';
 import { PlayerList } from '../';
 import logo from '../../images/logo.png';
 
@@ -9,33 +11,43 @@ import logo from '../../images/logo.png';
 export const WaitState = ({ id, onStart }) => {
     const navigate = useNavigate();
     const clearStore = useStore((state) => state.clearStore);
+    const [isShow, setIsShow] = useState(false);
 
 
     const copyToClipBoard = async copyMe => {
+        setIsShow(true);
         await navigator.clipboard.writeText(copyMe);
-        alert("Code Copied to clipboard");
+        setTimeout(() => {
+            setIsShow(false);
+        }, "700");
     };
 
     const [hostUser, username] = useStore(state => [state.hostname, state.username])
 
     return (
         <div className="waitState">
-            <img src={logo} width="200" alt="brain logo" class="mb-2" />
-            <h1 class="display-1">OTTOMH</h1>
-            <div>
-                <h2>Code:</h2>
-                {id}
-                <br/>
-                <Button onClick={() => copyToClipBoard(id)} variant="primary">Copy Room Code</Button>
+            <Modal className="copy-modal" show={isShow}>
+                <Modal.Body>Copied to clipboard!</Modal.Body>
+            </Modal>
+
+            <img src={logo} width="200" alt="brain logo" className="mb-2" />
+            <h1 className="display-1">OTTOMH</h1>
+
+            <div className="d-flex justify-content-center align-items-center gap-3">
+                <h2>Code: {id = 1234}</h2>
+                <Button className="copy-button" onClick={() => copyToClipBoard(id)} variant="primary">
+                    <i className="fa-solid fa-copy"></i>
+                </Button>
             </div>
-            <div>
-                <br />
+            
+            <div className="mt-5">
                 <h2>Players joined:</h2>
                 <PlayerList />
             </div>
+            
             <div className="d-flex justify-content flex-column align-items-center gap-3">
                 {hostUser === username ?
-                    <Button className="d-block" variant="primary" type="submit" onClick={onStart}>Start</Button> : null}
+                    <Button autoFocus className="d-block" variant="primary" type="submit" onClick={onStart}>Start</Button> : null}
                 <Button className="d-block" variant="primary" type="button" onClick={() => { clearStore(); navigate("/") }}>
                     Leave Lobby
                 </Button>
